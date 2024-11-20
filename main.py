d139deb5e0de0a51e88c340179052b3b031398b7
Andrii Kulbaba <andriykulbabapetrovych@gmail.com>
2024-11-20 14:50:52 +0200

    dfs algorithm to check is graph connected. Add more tests

--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def read_graph_file(filename: str) -> list[list[int, int, int]]:
     where:
     - u and v are integers representing the connected nodes (0-indexed),
     - weight is an integer representing the weight of the edge.
-
-    The graph is assumed to be undirected. The adjacency matrix is symmetrical, 
+    The graph is assumed to be undirected. The adjacency matrix is symmetrical,
     and all non-connected node pairs have a weight of 0.
 
     Args:
@@ -48,7 +47,6 @@ def read_graph_file(filename: str) -> list[list[int, int, int]]:
     for u, v, weight in edges:
         graph[u][v] = weight
         graph[v][u] = weight
-    
     return graph
 
 
@@ -58,9 +56,59 @@ def write_graph_file(filename: str, graph: Graph):
         writer.writerow(["Result"])
         writer.writerow([graph])
 
+def check_graph(graph:list[list[int, int, int]], start = 0) -> bool:
+    """
+    Checking graph connectivity using depth-first search.
+
+    The function analyzes the graph given by the adjacency matrix,
+    and determines whether all vertices can be reached
+    from one initial vertex.
+
+    Args:
+        graph (List[List[int]]): Graph adjacency matrix.
+            - Dimensionality: n x n, where n is the number of vertices
+
+        start (int, optional): The index of the starting vertex to search for.
+            Default is 0 (the first vertex in the graph).
+            Must be within [0, n-1].
+
+    Returns:
+        bool: Result of graph connectivity check.
+            - True: all vertices are reachable
+            - False: isolated vertices exist
+    >>> graph = [[0, 5, 3, 0], [5, 0, 0, 2], [3, 0, 0, 4], [0, 2, 4, 0]]
+    >>> check_graph(graph)
+    True
+    >>> graph = [[0, 1, 1, 0], [1, 0, 1, 0], [1, 1, 0, 0], [0, 0, 0, 0]]
+    >>> check_graph(graph)
+    False
+    >>> graph = [[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]
+    >>> check_graph(graph)
+    True
+    >>> graph =[[0, 1, 0, 0], [1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0]]
+    >>> check_graph(graph)
+    True
+    >>> graph = [[0, 1, 0, 0], [1, 0, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]]
+    >>> check_graph(graph)
+    False
+    """
+
+    n = len(graph)
+    visited = [False] * n
+
+    def dfs(vertex):
+        visited[vertex] = True
+        for neighbor in range(n):
+            if graph[vertex][neighbor] > 0 and not visited[neighbor]:
+                dfs(neighbor)
+
+    dfs(start)
+    return all(visited)
 
 def visualize_graph(graph: Graph):
     pass
+
+
 
 
 def main(graph_file, start_node, output_file):
